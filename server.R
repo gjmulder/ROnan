bfda744ab13c91b1a3bdba385ad1962aef4a6025
Gary Mulder <gjmulder@gmail.com>
2016-11-02 17:15:19 +0000

    Basic working UI and ggplot

--- server.R
+++ server.R
@@ -1,26 +1,60 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
+# This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
 library(shiny)
+library(ggplot2)
+library(dplyr)
 
-# Define server logic required to draw a histogram
+load('~/Work/dataset1.Rdata')
+
+# define server logic required to draw the time series
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
+  # input$date_range
+  
+  output$time_series_plot <- renderPlot({
+    str(input$date_range)
+    str(input$dbl_click)
+    str(input$plot_brush)
+    str(input$time_series_name)
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    if (!is.null(input$dbl_click)) {
+      # note anomaly
+      print(paste0("Anomaly at ", input$dbl_click))
+      return()
+    }
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # determine start and end date time ranges
+    start_datetime <- min(system_data$date.time)
+    end_datetime <- max(system_data$date.time)
     
+    if (is.list(input$plot_brush)) {
+      print("Brush area selected")
+      start_date_time <-
+        as.POSIXct(as.integer(input$plot_brush$xmin), origin = "1970-01-01")
+      end_date_time <-
+        as.POSIXct(as.integer(input$plot_brush$xmax), origin = "1970-01-01")
+    } else if (!is.null(input$date_range)) {
+      print("Date range selected")
+      start_date_time <-
+        as.POSIXct(input$date_range[1])
+      end_date_time <-
+        as.POSIXct(input$date_range[2])
+    }
+    
+    print(paste0("Start date : ", start_date_time))
+    print(paste0("End date   : ", end_date_time))
+    # Plot the data
+    system_data %>%
+      filter(date.time >= start_date_time &
+               date.time <= end_date_time) %>%
+      ggplot(aes_string(x = "date.time", y = input$time_series_name)) +
+      geom_point()
   })
   
 })
--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@
 # run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
@@ -11,23 +11,45 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("ROnan - manual exploration of time series"),
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(sidebarPanel(
+    dateRangeInput(
+      inputId = "date_range",
+      label = "Specify a date range, or Click + Drag an area of the plot",
+      start = "2016-01-01",
+      end = "2016-12-31"
     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+    selectInput(inputId = "time_series_name",
+                label = "Time Series",
+                choices = colnames(system_data)),
+    textInput(inputId = "annotation_text",
+              label = "Annotation",
+              value = "anomaly")
+  ),
+  
+  # 1. choose a time series
+  # 2. log or linear scale
+  # 3. double click to annotate with text
+  # 4. show annotations for the chosen time series
+  
+  #  . move forward an hour / day / week / month
+  #  . choose two time series
+  #  . colour with a function (e.g. is.na())
+  #  . geom_point or geom_line
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotOutput(
+      outputId = "time_series_plot",
+      dblclick = "dbl_click",
+      brush = brushOpts(
+        id = "plot_brush",
+        direction = "x",
+        resetOnNew = TRUE
+      )
     )
-  )
+  ))
 ))
