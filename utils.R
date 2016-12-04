d10f676a121dd1e0310389742e79dd65d28c84b5
Gary Mulder <gjmulder@gmail.com>
2016-12-04 21:09:55 +0000

    Vector autoregression model investigations

--- utils.R
+++ utils.R
@@ -1,52 +1,52 @@
-library(tidyverse)
-library(googlesheets)
-library(jsonlite)
-
-######################################################################################################
-# Load annotations from an .rdata file 
-# Assign whatever source data frame we want to examine to ts_df
-
+# library(tidyverse)
+# library(googlesheets)
+# library(jsonlite)
+# 
+# ######################################################################################################
+# # Load annotations from an .rdata file
+# # Assign whatever source data frame we want to examine to ts_df
+# 
 # # System data
 # load('~/Work/DS/dataset1.Rdata')
 # ts_df <-
 #   system_data
-
-# Cleaned system data
-ts_df <-
-  clean_ts_df
-
-print(summary(ts_df))
-
-######################################################################################################
-# Google Sheets preparation
-# n <- 5
-# filler <- matrix("-", nrow = n, ncol = n,
-#                  dimnames = list(NULL, paste0("V", seq_len(n))))
-
-## prepare the OAuth token and set up the target sheet:
-##  - do this interactively
-##  - do this EXACTLY ONCE
-
-# shiny_token <- gs_auth() # authenticate w/ your desired Google identity here
-# saveRDS(shiny_token, "shiny_app_google_sheet_token.rds")
-# ss <- gs_new("ts_annotations",
-#              row_extent = n, col_extent = n, input = filler)
-# ss$sheet_key # 1Y8MoUBi1CtzLNv0b_VPQS3MTAeapRE9ep5_7cMTnJUE
-
-## if you version control your app, don't forget to ignore the token file!
-## e.g., put it into .gitignore
-
-googlesheets::gs_auth(token = "~/Work/ronan/shiny_app_google_sheet_token.rds")
-sheet_key <-
-  "1Y8MoUBi1CtzLNv0b_VPQS3MTAeapRE9ep5_7cMTnJUE"
-gsheet_ts_annotations <-
-  googlesheets::gs_key(sheet_key)
-
-print(gs_ws_ls(gsheet_ts_annotations))
-
-###################################################################################################
-# Generate some reoccuring labels
-#
+# 
+# # # Cleaned system data
+# # ts_df <-
+# #   clean_ts_df
+# 
+# print(summary(ts_df))
+# 
+# ######################################################################################################
+# # Google Sheets preparation
+# # n <- 5
+# # filler <- matrix("-", nrow = n, ncol = n,
+# #                  dimnames = list(NULL, paste0("V", seq_len(n))))
+# 
+# ## prepare the OAuth token and set up the target sheet:
+# ##  - do this interactively
+# ##  - do this EXACTLY ONCE
+# 
+# # shiny_token <- gs_auth() # authenticate w/ your desired Google identity here
+# # saveRDS(shiny_token, "shiny_app_google_sheet_token.rds")
+# # ss <- gs_new("ts_annotations",
+# #              row_extent = n, col_extent = n, input = filler)
+# # ss$sheet_key # 1Y8MoUBi1CtzLNv0b_VPQS3MTAeapRE9ep5_7cMTnJUE
+# 
+# ## if you version control your app, don't forget to ignore the token file!
+# ## e.g., put it into .gitignore
+# 
+# googlesheets::gs_auth(token = "~/Work/ronan/shiny_app_google_sheet_token.rds")
+# sheet_key <-
+#   "1Y8MoUBi1CtzLNv0b_VPQS3MTAeapRE9ep5_7cMTnJUE"
+# gsheet_ts_annotations <-
+#   googlesheets::gs_key(sheet_key)
+# 
+# print(gs_ws_ls(gsheet_ts_annotations))
+# 
+# ###################################################################################################
+# # Generate some reoccuring labels
+# 
 # start_7am_an <-
 #   data_frame(date.time = seq(as.POSIXct(strptime("01/04/2016 00:00:00", "%d/%m/%Y %H:%M:%S", tz = "Europe/London")),
 #                              as.POSIXct(strptime("10/08/2016 00:00:00", "%d/%m/%Y %H:%M:%S", tz = "Europe/London")),
@@ -80,10 +80,77 @@ print(gs_ws_ls(gsheet_ts_annotations))
 #   trim = TRUE,
 #   verbose = TRUE
 # )
-
-###################################################################################################
-# Export for Anton
-#
+# 
+# ###################################################################################################
+# # Export for Anton
+# 
 # tses <- c("date.time", "desktop.reqs", "mobile.reqs", "desktop.orders", "mobile.orders")
 # system_data_ts <- system_data[, tses]
 # system_data_clean_ts <- clean_ts_df[, tses]
+
+###################################################################################################
+library(vars)
+library(urca)
+# library(parallel)
+# library(forecast)
+
+cluster <-
+  makeCluster(detectCores() / 2)
+
+col_names <-
+  c(
+    # "date.time",
+    # "monitor1.uptime",
+    # "monitor2.uptime",
+    # "funds.in.success",
+    # "funds.in.fail",
+    # "login.success",
+    # "login.fail",
+    "desktop.orders",
+    "desktop.reqs",
+    "mobile.orders",
+    "mobile.reqs"
+  )
+
+# clean_ts <-
+#   parLapply(cluster, clean_ts_df[, col_names], ts, frequency = 60 * 24 * 7)
+# 
+# system.time(
+#   result <-
+#     parLapply(cluster, clean_ts, nsdiffs)
+# )
+# stopCluster(cluster)
+# > result
+# $funds.in.success
+# [1] 0
+# 
+# $funds.in.fail
+# [1] 0
+# 
+# $login.success
+# [1] 0
+# 
+# $login.fail
+# [1] 0
+# 
+# $desktop.orders
+# [1] 0
+# 
+# $desktop.reqs
+# [1] 0
+# 
+# $mobile.orders
+# [1] 0
+# 
+# $mobile.reqs
+# [1] 0
+
+# jo_test <-
+#   ca.jo(clean_ts_df[, col_names], type="trace", K=1, ecdet="none", spec="longrun")
+# summary(jo_test)
+
+# var_select <-
+#   VARselect(clean_ts_df[, col_names], lag.max = 60, type = c("both"))
+
+var_fit <-
+  VAR(clean_ts_df[, col_names], type="const")
